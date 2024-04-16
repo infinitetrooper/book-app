ca40233ba27c8b6cd32a85b61588ab6246278109
Akhil Varma <akhil@appsmith.com>
2024-04-16 14:21:57 +0000

    Testing git granular status

---
+++ pages/Bugs and Issues/jsobjects/demoData/demoData.js
@@ -0,0 +1,328 @@
+export default {
+	data: {
+		records: [
+			{
+				id: "rec2uR9KTxgSQAM7D",
+				createdTime: "2023-07-06T07:32:41.000Z",
+				fields: {
+					Status: "Complete",
+					Attachments: [
+						{
+							id: "attvtQKs9PNvUX65R",
+							width: 330,
+							height: 241,
+							url: "https://v5.airtableusercontent.com/v1/18/18/1689062400000/CKR4mlttEv1_IEwbUMdslw/Kacl4TvDLmg8rq5g24IZDAJYfUJNshmDMff8hQ7FgCaFot3l-DTSaKlGG7KCQ1yOMkUfYP12CMXPrJ3XHubFQEVkSC9bwM3-_AjOl94sNV4Jz7WgqDIi1OOj0GFUBMAM/BM7GPaEjswi0nHbixClCfc39v36gXZ0_e1nDnV2Yc5o",
+							filename: "add-template-drop-down-menu.png",
+							size: 12073,
+							type: "image/png",
+							thumbnails: {
+								small: {
+									url: "https://v5.airtableusercontent.com/v1/18/18/1689062400000/mienED1pwEAM8mCK8uVTtg/TZSYh95tr_010GAFu6diUWASarGs52faJfSd71Eh_VMAgGPoz6UiYYja0EV0yYagBb6K7irdsgHeSIaCFsrcTw/otyXTzh9QL2HZ73lgqs3OliD0eERcQH3UPuic9Vm_hE",
+									width: 49,
+									height: 36
+								},
+								large: {
+									url: "https://v5.airtableusercontent.com/v1/18/18/1689062400000/i32XGPGegBY5ExmnN29TEA/NyZSYkDlrEE73GM8gI_azRDHID7SLO2BqWzzMoi8_1RbSkpyl8s2qkSVCwbquMeSvOEFqHOXNS3J0rmaa89F-Q/NoGEAe1oY32wsujTkOg9Vq7UkZG6CQij9xRLjIH-fUI",
+									width: 330,
+									height: 241
+								},
+								full: {
+									url: "https://v5.airtableusercontent.com/v1/18/18/1689062400000/mUBcqdkCHgZJc1Aavlw1kQ/ApYnNwekWKYwam-bX_AzTwtwCNgxijkzSx3jGjdC4Bc1jSafYwZARE2Fxi_BCAvKy_Baqm5kD0OeDa9FBvbxVw/GI2T5rqBckwh4Aqva-Umuy0va6dTN9Gc307svg3NndA",
+									width: 3000,
+									height: 3000
+								}
+							}
+						}
+					],
+					Days_old: 1473,
+					Assigned_to: "Bernard Casper",
+					Name: "Slight delay in receiving alerts from motion detection",
+					Description: "User reported that there was sometimes up to a 30 second or minute delay for an alert to be sent after PorchCam had detected activity (on the highest sensitivity settings)",
+					Closed: "1",
+					Bug_source: "Posted in the community forum by Deborah Markowski",
+					Associated_features: "Motion detection, Alerts",
+					Created_by: "Clara Rotelli",
+					Opened_datetime: "2019-06-24T18:41:40.000Z",
+					Priority: "High",
+					Opened_date: "06-24-19"
+				}
+			},
+			{
+				id: "rec7kMUbwKx267HNJ",
+				createdTime: "2023-07-06T07:32:41.000Z",
+				fields: {
+					Status: "Complete",
+					Attachments: [
+						{
+							id: "attBEb247bgk2w9S4",
+							width: 560,
+							height: 287,
+							url: "https://v5.airtableusercontent.com/v1/18/18/1689062400000/psqYXlHE-8qCzSadCwQWjQ/MOFDHUMAWFnQn1qbAAZp7f9CHLG0i1J6fTfNhEIRxoaHcg96ea_VWfa9xRSZ9NNx3zQ99MzqAa_zRn0EKFgPfTU63mRGIci0g9W2FQBJsIX0XYNYhBBR3Th6BWgwD2Zq/EF2Qe1PLvixD5Co14qlM4AJRGeMb64gJGQf3YSx4WPk",
+							filename: "template-config-file-content.png",
+							size: 21754,
+							type: "image/png",
+							thumbnails: {
+								small: {
+									url: "https://v5.airtableusercontent.com/v1/18/18/1689062400000/Px34fjTvbnHkTjWakyxR_A/YI9Ce5sN4b3GUa886ilZfyuzPSzo5Co1eZRsJOYaTdT1QmCePE1CxsTdjshuPYSDB7B7o91DmePlCCsDNBfhlg/p2xkNZ74t-DYP1GTQwNGUFqIGuPwrrcIFhQ-eo-NRYU",
+									width: 70,
+									height: 36
+								},
+								large: {
+									url: "https://v5.airtableusercontent.com/v1/18/18/1689062400000/FyxYn8ov-lbtRl45CCBR1Q/M6YOk-TEudOLCp_PT2aLjWTrA5WjZDFNuO1Zt0Y2W5-tTIxiJypdhMhfPq_RV6749qbeKAq3n_1EgxCbuPF2sw/afQU8otIjjpADiAgWDAnRl98jEftWYmgOUR9EqXj-nU",
+									width: 560,
+									height: 287
+								},
+								full: {
+									url: "https://v5.airtableusercontent.com/v1/18/18/1689062400000/VpR_UPKNRG5918Ajk8BbuQ/SGQ85kmbWrsZ5Zg3c5T928BQl2jag-jmYQOmDqT3D3Pn3xTEBTynWZt7WxNCeUWrj2cD3FIDzw4NhWEsLGykjg/fvcaYqCBrqqg1hwF8rmSfXsM2Ctg1va0yJukxqTeeAU",
+									width: 3000,
+									height: 3000
+								}
+							}
+						},
+						{
+							id: "attfZONsFa8t96j8U",
+							width: 330,
+							height: 241,
+							url: "https://v5.airtableusercontent.com/v1/18/18/1689062400000/R3kI5JWjAVCKFQ8R_gVbMg/2CaKH4WD4_DBW8Wp6xqDkJCGmOM5Z3EfwwuZusBkbpKi9kCUjyH8b81xUXeer-KBTGffEP1_SGWTiTBQmOuyeW5YB9hts7AIDS2OE8dDdGdrt_k8h6p3X_Igajgta4Ae/b3vwaF1tL48GMXhgBrzuW0Tx4i4k80DnSIgLFwgr_ys",
+							filename: "add-template-drop-down-menu.png",
+							size: 12073,
+							type: "image/png",
+							thumbnails: {
+								small: {
+									url: "https://v5.airtableusercontent.com/v1/18/18/1689062400000/4iF8A-q8RrRRAmk-8hQdgQ/1URXWbHPyl0G6GJLSHemn-r0TAO0CEnQmC9TauuhmREjuQ-yJg4gNKVKEyjbb6KrBoaJt0gNx1AIDo3_Cfa8yg/IugQ8ac-LPgK_hSvz4pWeJOn2a6G0afIf3Y2LATLNrc",
+									width: 49,
+									height: 36
+								},
+								large: {
+									url: "https://v5.airtableusercontent.com/v1/18/18/1689062400000/AK2_fvPZk-kQoLplWtWiyw/fVWjnkwYrTT9IHWnPmDRJ8AjAxGvaK2_XScNLgX5gaPiclCP57YP2qRLZO4ARhgn-Vl3MKnEMQHQRKDoZ6caYA/09PhNbLCeI6Yu_vrUXf--oyjrTPTzG0m7ESUq9y-Yu4",
+									width: 330,
+									height: 241
+								},
+								full: {
+									url: "https://v5.airtableusercontent.com/v1/18/18/1689062400000/dHX7tS9ltdX0UCeIP5JkpA/_zbKkWEjODm9zDw8Z6vhHsEKToTrSixke8rT-DsBPBpOlGl3rxQCRXrW6nIP-5e_FBRrbfwu8yOJjCQsdZel-g/S8dGn_6VVAZapjwjoBWrBg9eP2iTK0FUAezSTOJFaJo",
+									width: 3000,
+									height: 3000
+								}
+							}
+						}
+					],
+					Days_old: 1473,
+					Assigned_to: "Clara Rotelli",
+					Name: "Sending duplicates of every alert (to both text and email)",
+					Description: "User is receiving duplicates for each alert message, ranging from 2-5 additional messages on top of the original (and correct) alert to both email and text",
+					Closed: "1",
+					Bug_source: "Reported by user Kamala Davis (kamala.d123@example.com) on 11/13/16",
+					Associated_features: "Alerts",
+					Created_by: "Katina Frey",
+					Notified_users: true,
+					If_complete: "1",
+					Opened_datetime: "2019-06-24T18:41:37.000Z",
+					Priority: "High",
+					Opened_date: "06-24-19",
+					Notification_status: "✅ Already notified"
+				}
+			},
+			{
+				id: "recKRD8T8FC1T9twv",
+				createdTime: "2023-07-06T07:32:41.000Z",
+				fields: {
+					Status: "Duplicate",
+					Attachments: [
+						{
+							id: "atto4DG1tAi87SyNd",
+							width: 560,
+							height: 287,
+							url: "https://v5.airtableusercontent.com/v1/18/18/1689062400000/hF5OEMVpd32EToiV94ROxA/ugsL-GCJbkkrk-Kw6ZG-5hxpqgzR433Z-E9DbcCbmD0zNznJbmhLzwZt_jlF0LpqabvMZUANIuTJ5QwmImaYPS99b_LIGfAVWSRlX6a55xT3PDPf6QO_aAaBoS-SAFJ3/MQRPuPE3DJFFGHZrOjl2pQ8T__8RW7XRfWv4blB26Xk",
+							filename: "template-config-file-content.png",
+							size: 21754,
+							type: "image/png",
+							thumbnails: {
+								small: {
+									url: "https://v5.airtableusercontent.com/v1/18/18/1689062400000/yOFgUnX9WXcrJVwFWkXpOQ/VTcbnRIdZqMkX60E_YwsVenjVoEjuKgT3nOrwYL5dlXoCEJHquKvIN7RwL02UbWQkg0Y18kWihL_BpytDhjckQ/3u40ctlH92uv5NUk6HmeOKivnzvnHYdxe2bM4bAzpOg",
+									width: 70,
+									height: 36
+								},
+								large: {
+									url: "https://v5.airtableusercontent.com/v1/18/18/1689062400000/RVDJ5JKSQkqbPOVAraeLnw/Cw9O8u46Gz8jc6GIYZAUM7eje53eQ_4838CWinFrJs72BkrGXgfgLclATyd6SJ5sOSc3NI2JslX8ZrVism3j5A/GtnnOiYGyqg9bnbeeoUKRIZet41-LEPR_uu32Tc0ke8",
+									width: 560,
+									height: 287
+								},
+								full: {
+									url: "https://v5.airtableusercontent.com/v1/18/18/1689062400000/AiakFojmZxL06pR0MEsKHA/nwd8ayeAOJ8ufwIkL7ISkw2yOIXXZZH_3ef0PSUsWAID4ggrdQO9zg0Sb8TWHDcnAlf1orIoLhXaC2I3s6GWfw/VFIrz9FzbMsbTpciB9xt6Q_8YAFUgFRW-O1ABWVUCy8",
+									width: 3000,
+									height: 3000
+								}
+							}
+						},
+						{
+							id: "attkcdLYEeFiZGtiE",
+							width: 330,
+							height: 241,
+							url: "https://v5.airtableusercontent.com/v1/18/18/1689062400000/Ml3rKelU9qgt__vGqNV0qQ/xNbkNkU0rIFMaEeHO_McFO9tvfBGfyWlKBYH2WvgZ0bGfC927AUVhql7jkMFh0-h9Hj3TXX2bPygnlSrgdzdcpr8ujZvfwwsC2s22W-FchmdDukbkotfTn6EoLELyz3f/BwFsqMsh2sr5yj32293Il34kX3fOktEXDsNjrr_IXtQ",
+							filename: "add-template-drop-down-menu.png",
+							size: 12073,
+							type: "image/png",
+							thumbnails: {
+								small: {
+									url: "https://v5.airtableusercontent.com/v1/18/18/1689062400000/rVdJ-eQ3RkJ-umtKHIgYXQ/wumg7BQxa-2JJ-Yuz716cxi3lzkTL6Gl-Un1eX5y9V0K5mK5JN0Fn-3Y9swAE1ASQlTs83qaV18HizNcd239cg/NWO9HBeS1XWxUHab9yExwqryDkQuurn9n89UE-EmxkM",
+									width: 49,
+									height: 36
+								},
+								large: {
+									url: "https://v5.airtableusercontent.com/v1/18/18/1689062400000/PQ2kPhCXg5Haic_OMpBdsw/sz3roEwzoR-6Lf3VLfBKEjDmTDg1epjVRAMN0I12xHybB9AMjlcwPe0PNTZZzk26o7gyFy6M8V2l-v4ncBmsdA/qIfhGlfVJQIu4UY5VDfjPKVoTfVZdQFuOESt_wH5zIg",
+									width: 330,
+									height: 241
+								},
+								full: {
+									url: "https://v5.airtableusercontent.com/v1/18/18/1689062400000/8bmPXU1GYTMjNdi4qEAx2Q/GquzhNn2CUd9IDYcuYDjR9VRjEZejbyHZtLvydIDgqyX7MKF0H63N8inUcw9X8zM4gABC8QXIAgcQunduKIVnQ/tEOdRpD1BzRFkWkYSg2oiOmC8D6NnXbZyl11TzaM3lQ",
+									width: 3000,
+									height: 3000
+								}
+							}
+						}
+					],
+					Days_old: 1473,
+					Assigned_to: "Deepa Vartak",
+					Name: "Unable to open previously recorded videos until a restart of the app",
+					Description: "They shared that sometimes their app freezes on Android when they're trying to access previously recorded videos and they have to restart the app for it to work",
+					Closed: "1",
+					Bug_source: "Shared during a customer call w/ Elvis Production Co.",
+					Associated_features: "Recording, Android",
+					Created_by: "Katina Frey",
+					Opened_datetime: "2019-06-24T18:41:41.000Z",
+					Priority: "Low",
+					Opened_date: "06-24-19"
+				}
+			},
+			{
+				id: "recMc0531prb1AK7k",
+				createdTime: "2023-07-06T07:32:41.000Z",
+				fields: {
+					Status: "In progress",
+					Attachments: [
+						{
+							id: "attAmNhJ3XNW93rmO",
+							width: 560,
+							height: 287,
+							url: "https://v5.airtableusercontent.com/v1/18/18/1689062400000/XoAD1UNq7e4yPkd-JUjvEg/7Z3AdFsXsqUqDGh2JDA4ybQzu1YWsT3Kzl1HKeZDzMy12LhNbMqoWH80drXE5aTIsXUnjEPBLzMtGl9tHUkPA4g_Vv1kACNILr46XejKwHGymNP4lokvb168lotzMyzc/3UYGDJOg1Y5F9UPrNl2WCgSOrwwb5_zXhKrxhI3l-9M",
+							filename: "template-config-file-content.png",
+							size: 21754,
+							type: "image/png",
+							thumbnails: {
+								small: {
+									url: "https://v5.airtableusercontent.com/v1/18/18/1689062400000/ORkuBAMwE6ikEfh_StpTVg/2nbWzTEAiHUC5WkzGsX34rTW2HmaId1m4DPJTivv4eykkpd5SGQdwPxBLUhgvFADKpFUCM15q6PtzCfxGQmJuQ/433WzgTzXEq7A0DScgfzuOhGRDAhDbh9hhv9-exEdd8",
+									width: 70,
+									height: 36
+								},
+								large: {
+									url: "https://v5.airtableusercontent.com/v1/18/18/1689062400000/4kDT5OgmvgY2KaC-LoYOmQ/oDSAKFSVd9ubfGEZ7vWsozfllPc3hWrvMaMMe2AAHOk81zKjYYxjPJi48S7M5G5pWs9gzDlNF8VXgR13C9OJQQ/6LTZyVj7ofu5kDAvA3NVTryn-Z3NA2iZYP27Fic0acc",
+									width: 560,
+									height: 287
+								},
+								full: {
+									url: "https://v5.airtableusercontent.com/v1/18/18/1689062400000/PE597mc7wdGX-HqINAEHYg/bT8L5rs9pDPLpxxdOou5R2lep6AsOk5dTun3oyuOql8jmdYnNmkq3HQ-2CwBFNL7IVJSkKm9A3UfyRSETeMq5Q/YlEt1cSjU_s6ggniPPVaWv-Qif-Nt6LfW-r9_CLKO_k",
+									width: 3000,
+									height: 3000
+								}
+							}
+						}
+					],
+					Days_old: 1473,
+					Assigned_to: "Mattheus Anderson",
+					Name: "Battery life monitor is incorrect",
+					Description: "Battery life meter seems to be off by ~10%, as our PorchCam's battery ran off even though it said there was still 10% of charge left",
+					Bug_source: "Email from Narcissa Kairos (narci.k@example.com)",
+					Associated_features: "Battery",
+					Created_by: "Belinda Chen",
+					Opened_datetime: "2019-06-24T18:41:40.000Z",
+					Priority: "Normal",
+					Opened_date: "06-24-19"
+				}
+			},
+			{
+				id: "recSc4OuHYga4TxNW",
+				createdTime: "2023-07-06T07:32:41.000Z",
+				fields: {
+					Status: "Complete",
+					Attachments: [
+						{
+							id: "attXHZgZ7cX6u2BXH",
+							width: 3000,
+							height: 1465,
+							url: "https://v5.airtableusercontent.com/v1/18/18/1689062400000/FXdrWXxmyZ8H9oJxQj1o0Q/niRg4hK1N0t8H4evJ-3a5fytMJ9kWpiTSk2ckz8DSPwqXbUVElSdJ1wu9UaPMORPPAtIx1fRqOglV5G-liA52UwbH58Nh4hiPHaAOiCpoaY/1xRMLA9ePDYBOQVxjZPAvqZWd8-BPtHFxtAtap5i-ww",
+							filename: "uWmAGHr4ToqWy0uAAvbn_full_iphoneControl.png",
+							size: 4130570,
+							type: "image/png",
+							thumbnails: {
+								small: {
+									url: "https://v5.airtableusercontent.com/v1/18/18/1689062400000/6vWaNW99CYSd-tEFH6cSJQ/pz-H-lcJ0hdbK8xyuJherz4NcjBLJ_jUDY8GozeRwn7UNc1WxK3P31VUJ5mQeVBY5zxSbf06OaxoejQ3H-0qaK4w8arMYLoutIg3MVMJj28/vms_EA2x_sF0GYJMGrf5rDHEoeIJsnV30_ZxYRRAYSY",
+									width: 74,
+									height: 36
+								},
+								large: {
+									url: "https://v5.airtableusercontent.com/v1/18/18/1689062400000/g3gWDgG34GQxCFua7qOz3g/aXL-I7R2WP_L0vcdajzjj7hxgav-sNb7xMgn1gPxsBgSyJU_Q3XHH6-ai9sCYUFtybmO0KAOemtGGKVrcvZl9f2sdMXlRwewoKhf0xAkpPw/gjy5TA4FtOHg8V3ZXH6t-QHrZaN-w_qab5MjiB6Bfjg",
+									width: 512,
+									height: 512
+								},
+								full: {
+									url: "https://v5.airtableusercontent.com/v1/18/18/1689062400000/LXIdDOh3zX3eRX4aCFkvzQ/8nE4WEfyux53qxc5jgvAs_iIs5w6vPKUJysBJLXTFg6N7IqoDrWjAfS24smH5DFg29iEU7vNaJvQH5EbY07gGo7nZ9C0TG1BaeJLgZIehJA/xHf9uVa4laoI5P9M8NhwguYyECNE412ItlRme5LRbkk",
+									width: 3000,
+									height: 1465
+								}
+							}
+						},
+						{
+							id: "attej02nUzJvhVcKE",
+							url: "https://v5.airtableusercontent.com/v1/18/18/1689062400000/-jkb4UabGKzd2d1xIQgDLA/ZaTMgN9AJtFZ5KrOa32240T2OVNCpZ5yuROOx-bia0FzSv0b7VudryrQ0m5tEbeFF55HceTBB_dQlvWpTOLzMmLvgacfO0IZhz8UM-StNZA/D8TgcPmXQ5RQMRO1rockGIieGkNDHscAkvn6wafpWgk",
+							filename: "AirTableAttachments.mov",
+							size: 4936092,
+							type: "video/quicktime"
+						},
+						{
+							id: "attNLOjGaZpq80SrR",
+							width: 330,
+							height: 241,
+							url: "https://v5.airtableusercontent.com/v1/18/18/1689062400000/JlU0Z4rYk6NoJhlVJiuYyw/Q2u-O3hWtjwqQ98HSOH7jG3ovmo6v1oLXV9jhGtqUzz5v8dMl7BcGvyqfJCs6UalvvpS3eWuTrVt_ja32krj6kiRTwKwvLLxxb71HAMVsXlL5pIf-2pDoz_53DGSZmpY/K7y_SRHQCC5_ISlf9E4ftUfPbnwXeWg-VjekkzLSpts",
+							filename: "add-template-drop-down-menu.png",
+							size: 12073,
+							type: "image/png",
+							thumbnails: {
+								small: {
+									url: "https://v5.airtableusercontent.com/v1/18/18/1689062400000/Tly-iQ0PLNasbZLbhm-4QA/plev3OVnlQJGCAdEdYuNMtTLFjXQyvEVWs_KYc2rINHe_PNZkvOlLDv-4vyp0meLbh7A8C0yRFgu74BzY-FM-Q/UYefcP-7nnsXmT6OpzSZIOhzS9CVsYM88m7Fb9WM_n4",
+									width: 49,
+									height: 36
+								},
+								large: {
+									url: "https://v5.airtableusercontent.com/v1/18/18/1689062400000/JR7WV-rmWVjRwe0bMdeong/QXj7woRdVsFf7yjLRJbxdzkOlMAjUPN2JuTcGmcR59U5nCne_MqgD5uU5IxCu3fkum9c8JfQsLcLJtmzamrDKA/jCByiFUMuIRz1v631wgMEWEN0J102OHNOM922r_XRQE",
+									width: 330,
+									height: 241
+								},
+								full: {
+									url: "https://v5.airtableusercontent.com/v1/18/18/1689062400000/JNGV8r8wsOGYBCjHMA8Klw/3OAOl_wF3L57WryXGLzphXurM9x0IsdD3JLOJ5I0N9ApuKJsbDqpczHp7A451E0W4twWzxbPEAg73mDK8OHyfg/6xJ954pFoh1naQU61bC_bURBEPMI5VuTJWBk1hb0deA",
+									width: 3000,
+									height: 3000
+								}
+							}
+						}
+					],
+					Days_old: 1473,
+					Assigned_to: "Gabriella Lily",
+					Name: "iPhone is having issues pairing to a new PorchCam",
+					Description: "User is having issues pairing his iPhone to his PorchCam, though he's able to do so through an Android device or his desktop",
+					Closed: "1",
+					Bug_source: "Emailed issue from Karl Guo (ktotheguo@example.com)",
+					Associated_features: "iOS",
+					Created_by: "Belinda Chen",
+					If_complete: "1",
+					Opened_datetime: "2019-06-24T18:41:40.000Z",
+					Priority: "Critical",
+					Opened_date: "06-24-19",
+					Notification_status: "⚠️ Need to notify"
+				}
+			},
+		]
+	},
+	setData: (records) => {
+		this.data.records = records;
+	},
+}
