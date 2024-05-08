872e7dcef0b3b28a7d452fdd8e37938581d9f613
Akhil Varma <akhil@appsmith.com>
2024-05-08 12:20:12 +0000

    Home updated

--- pages/Home/jsobjects/main/main.js
+++ pages/Home/jsobjects/main/main.js
@@ -1,8 +1,14 @@
 export default {
 	async executeWorkflow() {
 		// Orchestrate your workflow below
-		console.log("Hello World 👋")
-		return true;
-		
+	 let a = 2;
+		this.add(a);
+		console.log(a);
+
+	},
+
+	add(a) {
+		a += 1;
+		console.log(a);
 	}
 }
